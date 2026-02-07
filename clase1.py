5e041cda238ec16dc561d93650642918d4e9664a
aristizabalhanna1-blip <aristizabalhanna1@gmail.com>
2026-02-07 16:35:54 +0000

    estoy guardamdo

---
+++ clase1.py
@@ -0,0 +1,17 @@
+edad = int(input ("Ingrese la edad"))
+print("La evaluacion e la expresioin es:", edad >= 18)
+if edad >= 18:
+    print("Si es permitido")
+else:
+    print("No es permitido") 
+
+
+
+
+
+
+
+
+
+
+
